a66cd55ea6c6eec4081952322f6e627da797f914
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-08-24 14:20:04 -0300

    fix:contact

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ ScrollReveal({
 ScrollReveal().reveal('.home-bio h1, .about-resume', { origin: 'left'});
 ScrollReveal().reveal('.home-bio p', { origin: 'right' });
 ScrollReveal().reveal('.home-bio, heading', { origin: 'top' });
-ScrollReveal().reveal('.profile-pic, .about-items, .skills-items, .projects-items, .form, .training', { origin: 'bottom' });
+ScrollReveal().reveal('.profile-pic, .about-items, .skills-items, .projects-items, .training-card, .socialContainer', { origin: 'bottom' });
 
 
 const animText = new Typed ('.animated-text', {
